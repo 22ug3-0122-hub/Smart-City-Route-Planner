1294ac7b69e81d46a1e7c7848a8a3eb4d06730c9
22ug3-0122-hub <22ug3-0122@sltc.ac.lk>
2025-10-20 21:20:48 +0530

    Smart City Route Planner.py

--- Smart City Route Planner.py
+++ Smart City Route Planner.py
@@ -2,8 +2,8 @@
 
 
 # - Group Members - #
-# 22ug3-0122 - K. G. S. Madusanka #
-# 22ug3-0474 - K. D. T. Ishinika  #
+# 22ug3-0122 - K. G. S. Madusanka
+# 22ug3-0474 - K. D. T. Ishinika
 # 22ug3-0604 - Kalana Helanjith 
 # 22ug3-0257 - Niranjan Wijebandara 
 
@@ -261,3 +261,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
